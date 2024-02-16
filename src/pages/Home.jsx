a0bf924f3ab8ead65860aa036c10d93cc7dff1b1
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-16 10:13:05 +0530

    Start room button and div created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Header from "../Components/Header";
 import DailyInfoCard from "../Components/DailyInfoCard";
 import style from "../style/Home.module.css";
 import RoomInfoCard from "../Components/RoomInfoCard";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsGrid3X3Gap } from "react-icons/bs";
 
 function Home() {
   return (
@@ -11,6 +13,15 @@ function Home() {
       <div className={style.home_container}>
         <DailyInfoCard />
         <RoomInfoCard />
+      </div>
+      <div className={style.action_btn}>
+        <button>
+          <AiOutlinePlus />
+          Start a room
+        </button>
+        <button>
+          <BsGrid3X3Gap />
+        </button>
       </div>
     </>
   );
